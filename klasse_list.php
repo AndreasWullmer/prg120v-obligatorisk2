49f97c1731715daaf5ef3f711004d29253e76bf1
Ditt Navn <andreas.wullum@gmail.com>
2025-10-27 23:39:22 +0100

    klasse_list: bruk fil-lager

--- klasse_list.php
+++ klasse_list.php
@@ -1,27 +1,26 @@
 <?php
-require 'db.php';
-$klasser = $pdo->query("SELECT klassekode, klassenavn, studiumkode FROM klasse ORDER BY klassekode")->fetchAll();
+require 'storage.php';
+$klasser = klasse_all();
 ?>
 <!doctype html>
 <html lang="no">
-<head><meta charset="utf-8"><title>Alle klasser</title></head>
+<head>
+  <meta charset="utf-8">
+  <title>Alle klasser</title>
+</head>
 <body>
   <h1>Alle klasser</h1>
-  <p><a href="index.php">Til meny</a></p>
-
   <?php if (!$klasser): ?>
-    <p>Ingen klasser registrert.</p>
+    <p>Ingen klasser registrert ennå.</p>
   <?php else: ?>
-    <table border="1" cellpadding="5" cellspacing="0">
-      <tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
+    <ul>
       <?php foreach ($klasser as $k): ?>
-        <tr>
-          <td><?php echo htmlspecialchars($k['klassekode']); ?></td>
-          <td><?php echo htmlspecialchars($k['klassenavn']); ?></td>
-          <td><?php echo htmlspecialchars($k['studiumkode']); ?></td>
-        </tr>
+        <li>
+          ID <?= (int)$k['id'] ?> – <?= htmlspecialchars($k['navn']) ?>
+        </li>
       <?php endforeach; ?>
-    </table>
+    </ul>
   <?php endif; ?>
+  <p><a href="index.php">Tilbake til meny</a></p>
 </body>
 </html>
