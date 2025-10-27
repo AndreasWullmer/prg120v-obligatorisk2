934a91d698d6ae1df865812383cdc46236abe2de
Ditt Navn <andreas.wullum@gmail.com>
2025-10-27 23:08:13 +0100

    legg inn mysqli-tilkobling

--- db.php
+++ db.php
@@ -1,19 +1,14 @@
 <?php
-die("TEST-MARKØR");
-// --- Verdier fra Dokploy ---
 $host = 'mysql-12.dokploy.usn.no';
 $port = 3306;
 $db   = 'anwul4724';
 $user = 'anwul4724';
 $pass = '4430anwul4724';
 
-// --- Koble til med mysqli ---
 $mysqli = new mysqli($host, $user, $pass, $db, $port);
 
-// Sjekk om det oppstår feil ved tilkobling
 if ($mysqli->connect_errno) {
     die("DB-tilkobling feilet: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
 }
 
-// Sørg for riktig tegnsett (æøå osv.)
 $mysqli->set_charset("utf8mb4");
